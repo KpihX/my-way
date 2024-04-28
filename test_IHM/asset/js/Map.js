4ded4297642cc6d9bb16a5097393fe54098f7043
NOMO-Gabriel <gabriel.nomo@facsciences-uy1.cm>
2024-04-28 13:11:10 +0100

    interface graphique presque prete

---
+++ test_IHM/asset/js/Map.js
@@ -0,0 +1,21 @@
+
+        // Toggle sidebar
+        document.getElementById('menuButton').addEventListener('click', function () {
+            var sidebar = document.getElementById('sidebar');
+            if (sidebar.style.display === 'none' || sidebar.style.display === '') {
+                sidebar.style.display = 'block';
+            } else {
+                sidebar.style.display = 'none';
+            }
+        });
+        
+        // Initialize the map
+        var map = L.map('map').setView([3.848, 11.502], 12); // Center the map on Yaoundé
+
+        // Add a tile layer (OpenStreetMap)
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            maxZoom: 19
+        }).addTo(map);
+
+    
